96892f91751cc7740bff4769793cc0ac1a89b6b1
mrstufford <mrstufford@gmail.com>
2011-04-20 16:03:08 +0000

    r1

--- js/1.0/vkontakteui.js
+++ js/1.0/vkontakteui.js
@@ -31,33 +31,30 @@ VK._ui = {
     size: {},
     drop: {},
     check: {},
-    radio: {},
-    date: {}
+    radio: {}
 };
 
 // Запрашиваем скрипты и стили
 (function () {
     try {
-        VK._ui.require(['http://vk.com/js/common.js', 'http://vk.com/js/lib/ui_controls.js', 'http://vk.com/js/al/datepicker.js'], 'script');
-        VK._ui.require(['http://vk.com/css/rustyle.css', 'http://vk.com/css/al/datepicker.css', 'http://vk.com/css/al/common.css', 'http://vk.com/css/ui_controls.css'], 'link');
+        VK._ui.require(['http://vk.com/js/common.js', 'http://vk.com/js/lib/ui_controls.js'], 'script');
+        VK._ui.require(['http://vk.com/css/rustyle.css', 'http://vk.com/css/al/common.css', 'http://vk.com/css/ui_controls.css'], 'link');
     } finally {
-
         /*
- * Пример использования: VK.box('box', 'My box', 500, ['box.php', {foo: 'bar'}], [{style: 'gray', label: 'Close', onClick: function(){VK.box('box', false);}}], {callback: onBoxCreate, onHide: function(){alert('Good luck!');}})
- * Манипуляции показать/скрыть бокс: VK.box('box', true/false);
- *
- * Name: первый аргумент, уникальное имя
- * Title: заголовок, конечно
- * Width: ширина бокса
- * Content: строка/массив с аргументами для загрузки по url
- * Buttons: массив, включающий объекты с параметрами
- * * Style: строка, gray/blue
- * * Label: строка
- * * onClick: функция
- * Options: объект с прочими параметрами
- * Autoposition: использовать ли автопозиционирование для приложений методом getScroll
- */
-
+         * Пример использования: VK.box('box', 'My box', 500, ['box.php', {foo: 'bar'}], [{style: 'gray', label: 'Close', onClick: function(){VK.box('box', false);}}], {callback: onBoxCreate, onHide: function(){alert('Good luck!');}})
+         * Манипуляции показать/скрыть бокс: VK.box('box', true/false);
+         *
+         * Name: первый аргумент, уникальное имя
+         * Title: заголовок, конечно
+         * Width: ширина бокса
+         * Content: строка/массив с аргументами для загрузки по url
+         * Buttons: массив, включающий объекты с параметрами
+         * * Style: строка, gray/blue
+         * * Label: строка
+         * * onClick: функция
+         * Options: объект с прочими параметрами
+         * Autoposition: использовать ли автопозиционирование для приложений методом getScroll
+         */
         VK.box = function (name, title, width, content, buttons, options, autoposition) {
             if (name && typeof title == 'boolean' && VK._ui.box[name])(title === true ? VK._ui.box[name].show() : VK._ui.box[name].hide());
             else {
@@ -106,23 +103,23 @@ VK._ui = {
             }
         };
         /*
- * Пример использования: VK.size('autosize', 0, {minHeight: 200});
- *
- * Name: уникальный идентификатор
- * Width: ширина поля
- * Options: объект с опциями
- */
+         * Пример использования: VK.size('autosize', 0, {minHeight: 200});
+         *
+         * Name: уникальный идентификатор
+         * Width: ширина поля
+         * Options: объект с опциями
+         */
         VK.size = function (name, width, options) {
             if (width) ge(name).style.width = width;
             VK._ui.size[name] = new Autosize(ge(name), options);
         };
         /*
- * Пример использования: VK.drop('dropdown', 200, [[1, 'Glock 18'],[2, 'Arctic Warfare Magnum', 'AWP. One shot – one kill.'],[3, 'HE Grenade', 'B-8-4. Maximal damage - 72HP.']]);
- *
- * Name: уникальный идентификатор
- * Data: массив с данными, формат: [[value, title[, descr]]]
- * Options: объект с опциями
- */
+         * Пример использования: VK.drop('dropdown', 200, [[1, 'Glock 18'],[2, 'Arctic Warfare Magnum', 'AWP. One shot – one kill.'],[3, 'HE Grenade', 'B-8-4. Maximal damage - 72HP.']]);
+         *
+         * Name: уникальный идентификатор
+         * Data: массив с данными, формат: [[value, title[, descr]]]
+         * Options: объект с опциями
+         */
         VK.drop = function (name, width, data, options) {
             // Dropdown можно использовать для создания autocomplete списка, просто пропишите в опциях autocomplete: true
             // В таком случае поиск будет происходить по данным, указанных в data, то есть без лишних запросов
@@ -131,15 +128,15 @@ VK._ui = {
             VK._ui.drop[name] = new Dropdown(ge(name), data, options);
         };
         /*
- * Пример использования: VK.auto('dropdown', 200, 'hints.php?type=json', {introText: 'Start typing', multiselect: true, defaultItems: [[1, 'Glock 18'], [2, 'Arctic Warfare Magnum']]});
- *
- * Name: уникальный идентификатор
- * Data: Ссылка для получения элементов
- * * Ответ должен быть в формате json, формат:
- * * [[value, title[, descr][, photo][, nonsense], [, nonsense]]]
- * Options: объект с параметрами
- * * cacheLength (int), defaultItems (array), introText (string), maxItems (int), maxItemsShown (int), multiselect (boolean), noResult (string), placeholder (string), placeholderColor (string, hex color), placeholderColored (boolean), showMax (int)
- */
+         * Пример использования: VK.auto('dropdown', 200, 'hints.php?type=json', {introText: 'Start typing', multiselect: true, defaultItems: [[1, 'Glock 18'], [2, 'Arctic Warfare Magnum']]});
+         *
+         * Name: уникальный идентификатор
+         * Data: Ссылка для получения элементов
+         * * Ответ должен быть в формате json, формат:
+         * * [[value, title[, descr][, photo][, nonsense], [, nonsense]]]
+         * Options: объект с параметрами
+         * * cacheLength (int), defaultItems (array), introText (string), maxItems (int), maxItemsShown (int), multiselect (boolean), noResult (string), placeholder (string), placeholderColor (string, hex color), placeholderColored (boolean), showMax (int)
+         */
         VK.auto = function (name, width, data, options) {
             if (!options) options = {};
             extend(options, {
@@ -149,15 +146,15 @@ VK._ui = {
             VK._ui.drop[name] = new Autocomplete(ge(name), data, options);
         };
         /*
- * Пример использования: VK.check('checkbox', 'Servers', 200);
- *
- * Name: уникальный идентификатор
- * Label: метка
- * Width: ширина
- * Checked: логическая конструкция: выбранный чек-бокс
- * Options: объект с опциями (checkedValue, notCheckedValue, onChange (value) и т. д.)
- * Display: возвращать видимым (true) или скрытым (false)
- */
+         * Пример использования: VK.check('checkbox', 'Servers', 200);
+         *
+         * Name: уникальный идентификатор
+         * Label: метка
+         * Width: ширина
+         * Checked: логическая конструкция: выбранный чек-бокс
+         * Options: объект с опциями (checkedValue, notCheckedValue, onChange (value) и т. д.)
+         * Display: возвращать видимым (true) или скрытым (false)
+         */
         VK.check = function (name, label, width, checked, options, display) {
             if ((VK._ui.check[name] || typeof name == 'object' || typeof name == 'array') && typeof label == 'boolean') {
                 if (typeof name == 'object' || typeof name == 'array') {
@@ -186,19 +183,19 @@ VK._ui = {
             };
         };
         /*
- * Пример использования: VK.radio('radiobutton', 'Flash', 200, 1);
- * Манипуляции над отображаемым/скрытым переключателем: VK.radio(['id1', 'id2', 'id3'], true/false);
- * Получение выбранного значения: VK.radio('radio');
- * * Вернет массив: [value, id]
- * Выбрать переключатель по его идентификатору: VK.radio('radiobutton', 'select');
- *
- * Name: уникальный идентификатор
- * Label: метка
- * Width: ширина
- * Checked: логическая конструкция: выбранный переключатель
- * Options: объект с опциями (select (value, name), deselect (value, name) и т. д.)
- * Display: возвращать видимым (true) или скрытым (false)
- */
+         * Пример использования: VK.radio('radiobutton', 'Flash', 200, 1);
+         * Манипуляции над отображаемым/скрытым переключателем: VK.radio(['id1', 'id2', 'id3'], true/false);
+         * Получение выбранного значения: VK.radio('radio');
+         * * Вернет массив: [value, id]
+         * Выбрать переключатель по его идентификатору: VK.radio('radiobutton', 'select');
+         *
+         * Name: уникальный идентификатор
+         * Label: метка
+         * Width: ширина
+         * Checked: логическая конструкция: выбранный переключатель
+         * Options: объект с опциями (select (value, name), deselect (value, name) и т. д.)
+         * Display: возвращать видимым (true) или скрытым (false)
+         */
         VK.radio = function (name, label, width, checked, options, display) {
             if (name && typeof label === 'undefined') {
                 each(geByClass('radiobtn_container'), function (i, v) {
@@ -235,20 +232,6 @@ VK._ui = {
                     VK.radio(name, display);
                 };
             };
-        };
-        /*
- * Пример использования: VK.date('datepicker', 200);
- *
- * Name: уникальный идентификатор
- * Width: ширина
- * Time: изначальное значения (unix)
- * Options: объект с опциями
- */
-        VK.date = function (name, width, time, options) {
-            ge(name).value = time ? time: Math.round(new Date().getTime() / 1000);
-            if (!options) options = {};
-            options.width = width;
-            VK._ui.date[name] = new Datepicker(name, options);
         };
     };
 })();
